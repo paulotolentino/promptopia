1ec7b8eb3c072d040802f183e9cec12fc4044fa2
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-07-14 04:22:23 -0300

    Set up PromptCard component

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { useRouter, usePathname } from "next/navigation";
 
-function PromptCard() {
-  return <div>PromptCard</div>;
+function PromptCard({ post, handleTagClick, handleEdit, handleDelete }) {
+  const [copy, setCopy] = useState("");
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(post.prompt);
+    setCopy(post.prompt);
+    setTimeout(() => {
+      setCopy("");
+    }, 3000);
+  };
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="User image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={
+              copy === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            alt="Copy icon"
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
 }
 
 export default PromptCard;
