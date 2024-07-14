d5f983688a91e9724e2f4005425b4ddf6b273faa
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-07-14 04:21:55 -0300

    Create get prompts endpoint

---
+++ app/api/prompt/route.js
@@ -0,0 +1,13 @@
+import Prompt from "@models/prompt";
+import { connectToDatabase } from "@utils/database"
+
+export const GET = async (req, res) => {
+
+  try {
+    await connectToDatabase();
+    const prompts = await Prompt.find().populate("creator");
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Request failed to get prompts", { status: 500 });
+  }
+}
